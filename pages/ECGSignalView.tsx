a40abeb5c83066a92a78bebfeeda71d1522dea6a
j4ger <xiayuxuan@live.com>
2024-01-10 15:50:37 +0800

    feat: simple QRS detection algorithm

--- pages/ECGSignalView.tsx
+++ pages/ECGSignalView.tsx
@@ -2,7 +2,7 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import SkiaChart, { SVGRenderer } from "@wuba/react-native-echarts/skiaChart";
 import { EChartsOption } from "echarts";
 import { LineChart } from "echarts/charts";
-import { GridComponent } from "echarts/components";
+import { GridComponent, MarkLineComponent } from "echarts/components";
 import * as echarts from "echarts/core";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import {
@@ -12,19 +12,23 @@ import {
   NativeModules,
   StyleSheet,
   useColorScheme,
+  View,
 } from "react-native";
 import { BleDisconnectPeripheralEvent, BleManagerDidUpdateValueForCharacteristicEvent } from "react-native-ble-manager";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Icon, Text } from "react-native-paper";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
 import { RootStackParamList } from "../App";
 import { BLEData, decodePacket } from "../utils/BLEData";
 import { DeviceContext, DeviceContextType } from "../utils/Device";
+import QRSDetector, { SAMPLE_RATE } from "../utils/QRSDetector";
 
 const BUFFER_SIZE = 600;
 const BLANK_SIZE = 50;
 const LINE_COLOR = "green";
-const DELAY_MS = 2;
+const DELAY_MS = 1;
 const MAX_QUEUE = 500;
+const UPDATE_RATIO = 0.8;
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
@@ -32,13 +36,16 @@ const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 type Props = { navigation: NativeStackNavigationProp<RootStackParamList, "ECGSignal"> };
 
 function ECGSignalView({ navigation }: Props): React.JSX.Element {
-  echarts.use([SVGRenderer, LineChart, GridComponent]);
+  echarts.use([SVGRenderer, LineChart, GridComponent, MarkLineComponent]);
 
   const chartRef = useRef<typeof SkiaChart | null>(null);
   let chart: echarts.ECharts | null = null;
 
   const data: [number, number][] = [];
   let queue: BLEData[] = [];
+  const markers: { xAxis: number }[] = [];
+  let lastPeak: number | null, averageRR: number | null = null;
+  const [beatRate, setBeatRate] = useState<number | null>(null);
 
   const colorScheme = useColorScheme();
 
@@ -74,6 +81,14 @@ function ECGSignalView({ navigation }: Props): React.JSX.Element {
           type: "line",
           showSymbol: false,
           color: LINE_COLOR,
+          markLine: {
+            symbol: "none",
+            animation: false,
+            label: {
+              show: false,
+            },
+            data: [],
+          },
         },
       ],
     };
@@ -125,40 +140,83 @@ function ECGSignalView({ navigation }: Props): React.JSX.Element {
     };
   }, [device]);
 
+  const shiftQueue = () => {
+    if (queue.length > MAX_QUEUE) {
+      queue = [];
+      Toast.show({ type: "error", text1: "Can't keep up. Too much data!" });
+      return;
+    }
+    if (queue.length > 0) {
+      const { timestamp, value } = queue.shift()!;
+      let cursor = timestamp % BUFFER_SIZE;
+      data[cursor] = [cursor, value];
+
+      const prev = data.slice(cursor + BLANK_SIZE);
+      const next = data.slice(0, cursor);
+
+      const QRSResult = QRSDetector(value, [...prev, ...next]);
+
+      if (cursor == markers[0]?.xAxis) {
+        markers.shift();
+      }
+
+      if (QRSResult) {
+        markers.push({ xAxis: cursor });
+        if (lastPeak) {
+          const rr = timestamp - lastPeak;
+          if (averageRR) {
+            averageRR = averageRR * UPDATE_RATIO + rr * (1 - UPDATE_RATIO);
+          } else {
+            averageRR = rr;
+          }
+          const RRSec = averageRR / SAMPLE_RATE;
+          setBeatRate(Math.floor(60 / RRSec));
+        }
+        lastPeak = timestamp;
+      }
+
+      chart?.setOption({
+        series: [
+          { name: "prev", data: prev },
+          {
+            name: "next",
+            data: next,
+            markLine: {
+              data: markers,
+            },
+          },
+        ],
+      });
+    }
+  };
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (queue.length > MAX_QUEUE) {
-        queue = [];
-        Toast.show({ type: "error", text1: "Can't keep up. Too much data!" });
-        return;
-      }
-      if (queue.length > 0) {
-        const newData = queue.shift()!;
-        let cursor = newData.timestamp % BUFFER_SIZE;
-        data[cursor] = [cursor, newData.value];
-
-        const prev = data.slice(cursor + BLANK_SIZE);
-        const next = data.slice(0, cursor);
-
-        chart?.setOption({
-          series: [
-            { name: "prev", data: prev },
-            { name: "next", data: next },
-          ],
-        });
-      }
-    }, DELAY_MS);
+    const interval = setInterval(shiftQueue, DELAY_MS);
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    console.log(`rate: ${beatRate}`);
+  }, [beatRate]);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SkiaChart ref={chartRef} />
-    </GestureHandlerRootView>
+    <>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <SkiaChart ref={chartRef} />
+        <View style={styles.center}>
+          <Icon size={40} source="heart" color={beatRate ? "red" : "grey"} />
+          <Text variant="displayMedium">{`BPM: ${beatRate ?? "--"}`}</Text>
+        </View>
+      </GestureHandlerRootView>
+    </>
   );
 }
 
 const colorScheme = Appearance.getColorScheme();
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  center: {
+    alignItems: "center",
+  },
+});
 
 export default ECGSignalView;
