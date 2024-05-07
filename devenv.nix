003eb6ccd31fc40ed4745861b082e2d2bdc60d81
j4ger <xiayuxuan@live.com>
2024-05-07 22:19:52 +0800

    feat: setup devenv for android build environment

--- devenv.nix
+++ devenv.nix
@@ -1,17 +1,53 @@
 # devenv.nix
-{ pkgs
-, inputs
-, lib
-, ...
-}: {
+{ inputs, pkgs, lib, config, ... }:
+let
+  nixpkgs = import inputs.nixpkgs {
+    system = pkgs.stdenv.system;
+  };
+
+  android-nixpkgs = import inputs.android-nixpkgs {
+    channel = "stable";
+  };
+
+  android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-34-0-0
+    platform-tools
+    platforms-android-34
+  ]);
+in {
   languages.dart.enable = true;
   languages.dart.package = pkgs.flutter;
 
-  packages = with pkgs; [ gnome.zenity yad sqlite ];
+  env.ANDROID_HOME = "${android-sdk}/share/android-sdk";
+  env.ANDROID_SDK_ROOT = "${android-sdk}/share/android-sdk";
+
+  languages.java = {
+    enable = true;
+    gradle.enable = true;
+    jdk.package = pkgs.jdk;
+  };
+
+  packages = with pkgs; [ gnome.zenity yad sqlite jdk android-sdk android-tools ];
 
   env.LD_LIBRARY_PATH = "${lib.makeLibraryPath [ pkgs.sqlite ]}";
 
   enterShell = ''
+    set -e
+
+    ANDROID_USER_HOME=$(pwd)/.android
+    ANDROID_AVD_HOME=$(pwd)/.android/avd
+
+    export PATH="${android-sdk}/bin:$PATH"
+
+    export ANDROID_USER_HOME
+    export ANDROID_AVD_HOME
+
+    test -e "$ANDROID_USER_HOME" || mkdir -p "$ANDROID_USER_HOME"
+    test -e "$ANDROID_AVD_HOME" || mkdir -p "$ANDROID_AVD_HOME"
+
+    set +e
+
     flutter --version
   '';
 }
